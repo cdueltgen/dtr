50abadbc3b457f51950bb9a650b28bae4d5d9964
cdueltgen <cdueltgen@gmail.com>
2013-10-17 00:22:38 -0700

    testing sleep and getting update tweets

--- test-tweepy.py
+++ test-tweepy.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from keys import *
 
 import tweepy
@@ -6,6 +8,19 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_SECRET)
 
 api = tweepy.API(auth)
+
+def sleepy():
+    timeline = api.home_timeline()
+    timeline.reverse()
+    print timeline[-1].id
+    while True:
+        sleep(30)
+        new_tweets = api.home_timeline(since_id=timeline[-1].id)
+        print new_tweets
+        if new_tweets is not None:
+            timeline.extend(new_tweets.reverse())
+        # timeline.extend(new_tweets.reverse())
+        # print timeline[-1].id
 
 # This will get the last 20 tweets, latest most recent at position 0
 # dump = api.home_timeline()
